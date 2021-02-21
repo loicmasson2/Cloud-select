c839f9054ac94f553a1ce93c93e1c1bd3090aa7e
Loïc Masson <masson.loic.023@gmail.com>
2021-02-21 16:15:37 +0200

    feat(back): add endpoint to get the closest instance from the user wth hardcoded values from helsinki

--- cloud-select-back/app.py
+++ cloud-select-back/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import make_response
 from flask import jsonify
 from requests import get
+from math import cos, asin, sqrt
 
 app = Flask(__name__)
 SITE_NAME = "https://api.aiven.io/v1"
@@ -54,6 +55,13 @@ def aiven_cloud_instances(provider_name, region):
     return jsonify(result)
 
 
+@app.route("/clouds/closest")
+def aiven_closest():
+    v = {"geo_latitude": 60.158, "geo_longitude": 24.903}
+    clouds_json = get(f"{SITE_NAME}/clouds").json()
+    return jsonify(closest(clouds_json["clouds"], v))
+
+
 @app.route("/<page_name>")
 def other_page(page_name):
     response_message = "The page named %s does not exist." % page_name
@@ -63,6 +71,33 @@ def other_page(page_name):
 
 def trim_provider_name(provider_name):
     return provider_name.split("-")[0]
+
+
+def distance(lat1, lon1, lat2, lon2):
+    print(lat1)
+    print(lon1)
+    print(lat2)
+    print(lon2)
+
+    p = 0.017453292519943295
+    a = (
+        0.5
+        - cos((lat2 - lat1) * p) / 2
+        + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    )
+    return 12742 * asin(sqrt(a))
+
+
+def closest(data, v):
+    return min(
+        data,
+        key=lambda p: distance(
+            v["geo_latitude"],
+            v["geo_longitude"],
+            p["geo_latitude"],
+            p["geo_longitude"],
+        ),
+    )
 
 
 if __name__ == "__main__":
