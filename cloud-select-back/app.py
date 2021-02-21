2a0fc116951a395023632ceb87300774c85bace0
Loïc Masson <masson.loic.023@gmail.com>
2021-02-21 11:48:08 +0200

    feat(back): add an endpoint to get a list of cloud providers and add few tests to compare raw json and a sub function

--- cloud-select-back/app.py
+++ cloud-select-back/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import make_response
+from flask import jsonify
 from requests import get
+import json
+
 
 app = Flask(__name__)
 SITE_NAME = "https://api.aiven.io/v1"
@@ -8,13 +11,29 @@ SITE_NAME = "https://api.aiven.io/v1"
 
 @app.route("/clouds")
 def aiven_clouds():
-    return get(f"{SITE_NAME}/clouds").content
+    return jsonify(get(f"{SITE_NAME}/clouds").json())
+
+
+@app.route("/providers")
+def aiven_providers():
+    providers = []
+    with open("clouds.json") as json_file:
+        clouds_json = json.load(json_file)
+        for p in clouds_json["clouds"]:
+            provider_name = trim_provider_name(p["cloud_name"])
+            if provider_name not in providers:
+                providers.append(provider_name)
+    return jsonify(providers)
 
 
 @app.route("/<page_name>")
 def other_page(page_name):
     response = make_response("The page named %s does not exist." % page_name, 404)
     return response
+
+
+def trim_provider_name(provider_name):
+    return provider_name.split("-")[0]
 
 
 if __name__ == "__main__":
--- cloud-select-back/app.test.py
+++ cloud-select-back/app.test.py
@@ -1,18 +1,30 @@
 import unittest
 import json
 
-from app import app
+from app import app, trim_provider_name
 
 
 class BasicTestCase(unittest.TestCase):
     def test_clouds(self):
         tester = app.test_client(self)
-        response = tester.get("/clouds", content_type="html/text")
+        response = tester.get("/clouds", content_type="application/json")
         self.assertEqual(response.status_code, 200)
         with open("clouds.json") as json_file:
             actual_json = json.dumps(json.loads(response.data), sort_keys=True)
             expected_json = json.dumps(json.load(json_file), sort_keys=True)
             self.assertEqual(expected_json, actual_json)
+
+    def test_providers(self):
+        tester = app.test_client(self)
+        response = tester.get("/providers", content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        with open("providers.json") as json_file:
+            actual_json = json.dumps(json.loads(response.data), sort_keys=True)
+            expected_json = json.dumps(json.load(json_file), sort_keys=True)
+            self.assertEqual(expected_json, actual_json)
+
+    def test_trim_provider_name(self):
+        self.assertEqual("aws", trim_provider_name("aws-af-south-1"))
 
     def test_other(self):
         tester = app.test_client(self)
