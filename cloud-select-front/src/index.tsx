3826b96b631d2216941556247d6b5c926a93c8da
Loïc Masson <masson.loic.023@gmail.com>
2021-02-23 18:09:50 +0200

    feat(front): add react router dependency

--- cloud-select-front/src/index.tsx
+++ cloud-select-front/src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Routing = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
+        </Router>
+    );
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Routing />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
