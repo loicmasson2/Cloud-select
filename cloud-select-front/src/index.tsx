3a451030c70a593776122f0af7b1ce81a72fbfa9
Loïc Masson <masson.loic.023@gmail.com>
2021-02-26 20:31:41 +0200

    fix(front): still trying to fix the docker issue

--- cloud-select-front/src/index.tsx
+++ cloud-select-front/src/index.tsx
@@ -1,7 +1,5 @@
-// @ts-nocheck
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import reportWebVitals from 'reportWebVitals';
 
 import App from 'App';
